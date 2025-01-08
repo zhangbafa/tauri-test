64fdca57bb4f6de24b2ecde12ae4f9d4bb27e88e
zhangbafa <8069570+hubangtuan@user.noreply.gitee.com>
2025-01-08 16:13:10 +0800

    add update

--- src/components/shop.vue
+++ src/components/shop.vue
@@ -140,8 +140,7 @@ input[type=number] {
 <script setup>
 import { reactive, ref, onMounted } from 'vue';
 import { Message } from '@arco-design/web-vue';
-import { check } from '@tauri-apps/plugin-updater';
-import { relaunch } from '@tauri-apps/plugin-process';
+
 // Create reactive references for ROI and profit margin
 const breakEvenROI = ref('0')
 const profitMargin = ref('0')
@@ -428,33 +427,5 @@ onMounted(() => {
   darkModeQuery.addEventListener('change', handleThemeChange);
 })
 
-const updateApp = async () => {
-  const update = await check();
-  if (update) {
-      console.log(
-          `found update ${update.version} from ${update.date} with notes ${update.body}`
-      );
-      let downloaded = 0;
-      let contentLength = 0;
-      // alternatively we could also call update.download() and update.install() separately
-      await update.downloadAndInstall((event) => {
-          switch (event.event) {
-              case 'Started':
-                  contentLength = event.data.contentLength;
-                  console.log(`started downloading ${event.data.contentLength} bytes`);
-                  break;
-              case 'Progress':
-                  downloaded += event.data.chunkLength;
-                  console.log(`downloaded ${downloaded} from ${contentLength}`);
-                  break;
-              case 'Finished':
-                  console.log('download finished');
-                  break;
-          }
-      });
 
-      console.log('update installed');
-      await relaunch();
-  }
-}
 </script>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,11 +1,58 @@
-<script setup lang="ts">
+<script setup lang="js">
+import { ref } from 'vue';
+import { check } from '@tauri-apps/plugin-updater';
+import { relaunch } from '@tauri-apps/plugin-process';
 import Shop from '../components/shop.vue';
 import Daren from '../components/daren.vue';
+const visible = ref(false);
+const download = ref('');
+const checkVersion = async () => {
+  const update = await check();
+  if (update) {
+    console.log(
+      `found update ${update.version} from ${update.date} with notes ${update.body}`
+    );
+    let downloaded = 0;
+    let contentLength = 0;
+    visible.value = true;
+    await update.downloadAndInstall((event) => {
+      switch (event.event) {
+        case 'Started':
+          contentLength = event.data.contentLength;
+          download.value = '准备下载'
+          console.log(`started downloading ${event.data.contentLength} bytes`);
+          break;
+        case 'Progress':
+          downloaded += event.data.chunkLength;
+          download.value = '已下载' + Math.round((downloaded / contentLength) * 100) + '%';
+          console.log(`downloaded ${downloaded} from ${contentLength}`);
+          break;
+        case 'Finished':
+          download.value = '下载完成，点击确定重启更新';
+          break;
+      }
+    });
+
+    console.log('update installed');
+
+  }
+}
+checkVersion()
+
+
+
+const handleOk = async () => {
+  visible.value = false;
+  await relaunch();
+};
+const handleCancel = () => {
+  visible.value = false;
+}
 </script>
 
 <template>
-    <a-tabs :animation="true" :justify="true" size="large" :destroy-on-hide="true" :lazy-load="true">
-    <a-tab-pane key="1" >
+  <a-tabs :animation="true" :justify="true" size="large" :destroy-on-hide="true" :lazy-load="true">
+    <a-tab-pane key="1">
       <template #title>
         <icon-apps /> 商家版
       </template>
@@ -18,12 +65,18 @@ import Daren from '../components/daren.vue';
       <Daren />
     </a-tab-pane>
   </a-tabs>
+  <a-modal v-model:visible="visible" @ok="handleOk" @cancel="handleCancel">
+    <template #title>
+      更新提示
+    </template>
+    <div>有新的更新可用 {{}}</div>
+  </a-modal>
 </template>
 
 <style>
-.arco-tabs-nav-tab{
-    display: flex;
-    justify-content: center;
-    font-size:16px;
+.arco-tabs-nav-tab {
+  display: flex;
+  justify-content: center;
+  font-size: 16px;
 }
 </style>
